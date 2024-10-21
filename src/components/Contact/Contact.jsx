75974f2e1c11ddb14d30998cd0a62b8a3b13dc91
Retep-dev <afolabiengopet@gmail.com>
2024-10-21 23:13:44 +0100

    second commit portfolio

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">engopee@gmail.com</a>
+          <a href="mmailto:engopee@gmail.com">engopee@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
